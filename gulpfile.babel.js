43c4304fc71096f6d623cd4690cfc656ec7982e2
Kamil Wozniak <kamil.m.wozniak@gmail.com>
2016-03-23 09:48:03 +0000

    Refactoring

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,25 +1,25 @@
-var fs = require('fs');
-var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
-var babel = require('gulp-babel');
-var concat = require('gulp-concat');
-var wrap = require('gulp-wrap');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var filter = require('gulp-filter');
-var sass = require('gulp-sass');
-var cache = require('gulp-cached');
-var remember = require('gulp-remember');
-var deleteLines = require('gulp-delete-lines');
-var stream_queue = require('streamqueue');
-var minifyCss = require('gulp-minify-css');
-var prefix = require('gulp-autoprefixer');
+var fs = require('fs'),
+    gulp = require('gulp'),
+    sourcemaps = require('gulp-sourcemaps'),
+    babel = require('gulp-babel'),
+    concat = require('gulp-concat'),
+    wrap = require('gulp-wrap'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    filter = require('gulp-filter'),
+    sass = require('gulp-sass'),
+    cache = require('gulp-cached'),
+    remember = require('gulp-remember'),
+    deleteLines = require('gulp-delete-lines'),
+    stream_queue = require('streamqueue'),
+    minifyCss = require('gulp-minify-css'),
+    prefix = require('gulp-autoprefixer'),
 
-var client_src_path = 'app/client_src';
+    src_path = 'src',
 
-var src_files = [
-    'app',
-].map(to_client_src_path);
+    src_files = [
+        'app',
+    ].map(to_src_path);
 
 gulp.task('default', make_bundler(src_files, 'bundle.js'));
 
@@ -62,23 +62,23 @@ function handle_stream_error(error_name) {
     };
 }
 
-function to_client_src_path(filename) {
-    return 'app/client_src/js/' + filename + '.js';
+function to_src_path(filename) {
+    return 'src/js/' + filename + '.js';
 }
 
 gulp.task('watch', ['default', 'styles'], function () {
     gulp.watch(src_files, ['default']);
-    gulp.watch('app/client_src/scss/*.scss', ['styles']);
+    gulp.watch('src/scss/*.scss', ['styles']);
     gulp.watch(ractive_component_template_files, ['default']);
 });
 
 var paths = {
-    sass: 'app/client_src/scss/',
-    css: 'app/static/css/'
+    sass: 'src/scss/',
+    css: 'static/css/'
 };
 
 gulp.task('styles', function(){
-    gulp.src(client_src_path + '/scss/*.scss')
+    gulp.src(src_path + '/scss/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(prefix({
                 browsers: ['last 2 versions'],
@@ -86,12 +86,12 @@ gulp.task('styles', function(){
             }))
         .pipe(minifyCss({compatibility: 'ie8'}))
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('app/static/css/'));
-    gulp.src(client_src_path + '/scss/*.scss')
+        .pipe(gulp.dest('static/css/'));
+    gulp.src(src_path + '/scss/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(prefix({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('app/static/css/'));
+        .pipe(gulp.dest('static/css/'));
 });
